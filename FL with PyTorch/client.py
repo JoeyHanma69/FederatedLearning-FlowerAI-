17d686f41f471996446a0331e87d3875bd864192
Joseph Linao <linaojoseph3111@gmail.com>
2025-03-30 22:04:25 +1100

    chaewonLockedIn

--- FL with PyTorch/client.py
+++ FL with PyTorch/client.py
@@ -1,7 +1,7 @@
 from centralised import load_model, load_data, train, test 
 from collections import OrderedDict 
 import torch
-import flwr as flwr 
+import flwr as fl 
 
 def set_parameters(model, paramaters):  
     params_dict = zip(model.state_dict().keys(), paramaters) 
@@ -12,4 +12,23 @@ def set_parameters(model, paramaters):
 net = load_model() 
 trainloader, testloader = load_data() 
 
+class FlowerClient(fl.client.NumPyClient):  
+    def get_parameters(self, config): 
+        return [val.cpu().numpy() for _, val in net.state_dict().items()]  
+    
+    def fit(self, parameters, config): 
+        set_parameters(net, parameters)  
+        train(net, trainloader, epochs=1)  
+        return self.get_parameters({}), len(trainloader.dataset), {} 
+    
+    def evaluate(self, parameters, config): 
+        set_parameters(net, parameters)  
+        loss, accuracy = test(net, testloader)
+        return float(loss), len(testloader.dataset), {"accuracy" : accuracy} 
+    
+fl.client.start_numpy_client( 
+    server_address="127.0.0.1:8080", 
+    client=FlowerClient()         
+)
+        
     
