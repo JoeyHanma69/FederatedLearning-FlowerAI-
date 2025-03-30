90e7c4755758374a16733ae382e2bbb0f1f93d46
Joseph Linao <linaojoseph3111@gmail.com>
2025-03-30 21:50:20 +1100

    Update client.py

--- FL with PyTorch/client.py
+++ FL with PyTorch/client.py
@@ -1,4 +1,4 @@
-from centralised import load_model, load_model, train, test 
+from centralised import load_model, load_data, train, test 
 from collections import OrderedDict 
 import torch
 import flwr as flwr 
@@ -6,5 +6,10 @@ import flwr as flwr
 def set_parameters(model, paramaters):  
     params_dict = zip(model.state_dict().keys(), paramaters) 
     state_dict = OrderedDict({torch.K: torch.tensor(v) for k, v in params_dict}) 
-    model.load_state_dict(state_dict, strict=True)
+    model.load_state_dict(state_dict, strict=True) 
+    return model 
+
+net = load_model() 
+trainloader, testloader = load_data() 
+
     
