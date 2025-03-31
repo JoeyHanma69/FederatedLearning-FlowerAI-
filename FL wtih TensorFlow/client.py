180454e90f678cd75470ae2d214524729944034d
Joseph Linao <linaojoseph3111@gmail.com>
2025-03-31 19:37:46 +1100

    update client.py

--- FL wtih TensorFlow/client.py
+++ FL wtih TensorFlow/client.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import flwr as fl
 
 
-model = tf.keras.application.MobileNetV2((32, 32, 3), classes=10, weights=None) 
+model = tf.keras.applications.MobileNetV2((32, 32, 3), classes=10, weights=None) 
 model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"]) 
 (x_train, y_train), (x_test, y_test) =tf.keras.datasets.cifar10.load_data() 
 
